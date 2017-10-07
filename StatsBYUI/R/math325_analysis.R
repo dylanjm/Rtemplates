e3963292ec5be878dbe2efadfd678257a0c12b6e
Dylan McDowell <dylan.mcdowell226@gmail.com>
2017-10-06 22:29:51 -0600

    added code comments

--- StatsBYUI/R/math325_analysis.R
+++ StatsBYUI/R/math325_analysis.R
@@ -1,3 +1,18 @@
+# Package: StatsBYUI
+# Template: Math 325 Analysis (HTML)
+# Dylan McDowell
+# October 6th, 2017
+#
+# Custom formatting function
+# used to abstract away YAML settings from students.
+# Creates a minimalistic YAML in the template and also 
+# allows for quick changes and better customization.
+#
+# TODO:
+# - Integrate CSS formatting
+# - Develop a possible HTML header 
+# - Include Data Science Logo .png in the header.
+
 math325_analysis <- function() {
   # get the locations of resource files located within the package
   # css <- system.file("reports/styles.css", package = "Math325")
--- StatsBYUI/R/math335_analysis.R
+++ StatsBYUI/R/math335_analysis.R
@@ -1,3 +1,18 @@
+# Package: StatsBYUI
+# Template: Math 335 Analysis (HTML)
+# Dylan McDowell
+# October 6th, 2017
+#
+# Custom formatting function
+# used to abstract away YAML settings from students.
+# Creates a minimalistic YAML in the template and also 
+# allows for quick changes and better customization.
+#
+# TODO:
+# - Integrate CSS formatting
+# - Develop a possible HTML header 
+# - Include Data Science Logo .png in the header.
+
 math335_analysis <- function() {
 
   # call the base html_document function
