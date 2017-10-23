078d80137b84b6191e20bab42962cffd441e824b
Dylan McDowell <dylan.mcdowell226@gmail.com>
2017-10-23 13:33:29 -0600

    Homework Template

---
+++ HomeworkBYUI/R/homework_pdf.R
@@ -0,0 +1,24 @@
+# Package: HomeworkBYUI
+# Template: Homework Template (PDF)
+# Dylan McDowell
+# October 23rd, 2017
+#
+# Custom formatting function
+# used to abstract away YAML settings from students.
+# Creates a minimalistic YAML in the template and also
+# allows for quick changes and better customization.
+#
+# TODO:
+# - Develop a possible TeX header
+
+homework_pdf <- function() {
+  template_file = system.file('rmarkdown',
+                              'templates',
+                              'homework',
+                              'resources',
+                              'homework_template.tex',
+                              package = "HomeworkBYUI")
+
+  # call the base pdf_document function
+  rmarkdown::pdf_document(template = template_file, highlight = "tango")
+}
