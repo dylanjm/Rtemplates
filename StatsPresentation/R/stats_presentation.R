5d893f2432459d93c8b9e88e811a1c828e84cbd1
Dylan McDowell <dylan.mcdowell226@gmail.com>
2018-02-12 19:13:02 -0700

    Presentation

---
+++ StatsPresentation/R/stats_presentation.R
@@ -0,0 +1,26 @@
+# Package: Stats Presentation
+# Template: Homework Template (Beamer)
+# Dylan McDowell
+# February 12th, 2018
+#
+# Custom formatting function
+# used to abstract away YAML settings from students.
+# Creates a minimalistic YAML in the template and also
+# allows for quick changes and better customization.
+#
+# TODO:
+# - Develop a possible TeX header
+
+stats_presentation <- function() {
+  template_file = system.file('rmarkdown',
+                              'templates',
+                              'report',
+                              'resources',
+                              'beamer.tex',
+                              package = "StatsPresentation")
+
+  # call the base pdf_document function
+  rmarkdown::beamer_presentation(template = template_file,
+                          highlight = "kate",
+                          latex_engine = "pdflatex")
+}
