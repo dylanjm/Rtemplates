1417ef38c59d3917b56c8e998701231d94f67f91
Dylan McDowell <dylan.mcdowell226@gmail.com>
2018-02-12 23:10:17 -0700

    changes

--- StatsPresentation/R/stats_presentation.R
+++ StatsPresentation/R/stats_presentation.R
@@ -19,29 +19,8 @@ stats_presentation <- function() {
                               'beamer.tex',
                               package = "StatsPresentation")
 
-  img_dir  <- system.file("resources", "images", package = "StatsPresentation")
-
-  pre_processor <- function(metadata, input_file, runtime, knit_meta,
-                            files_dir, output_dir) {
-    if (!file.exists(files_dir))
-      dir.create(files_dir)
-    file.copy(img_dir,  files_dir, recursive = TRUE)
-  }
-
-  res <- rmarkdown::output_format(
-    knitr = NULL,
-    pandoc = NULL,
-    pre_processor = pre_processor,
-    ## Note that here `theme` and `highlight` are just parameters to make
-    ## the HTML document tiny
-    ## The real `theme` and `highlight` passed to html_pretty() are
-    ## reflected in the final CSS file. Here `css` is set to NULL, but
-    ## it will be set in the pre_processor() hook
-    base_format = rmarkdown::beamer_presentation(template = template_file,
-                                                   highlight = "kate",
-                                                   latex_engine = "pdflatex")
-  )
-
-  res
+  base_format = rmarkdown::beamer_presentation(template = template_file,
+                                               highlight = "kate",
+                                               latex_engine = "pdflatex")
 
 }
